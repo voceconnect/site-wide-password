437cbe1f790b5a85757f9e2ce17d143f56f33f8b
Sean McCafferty <smccafferty@voceconnect.com>
2016-01-15 12:43:17 -0500

    Adding template changes from action gram repo

--- src/tmpl/password.php
+++ src/tmpl/password.php
@@ -1,23 +1,27 @@
 <?php get_header(); ?>
 <div class="site-main swp-password-template">
-	<main class="content-area-full-width" role="main">
-		<div class="post-well">
-			<h2>Password Protected</h2>
-			<form method="post">
-				<div class="swp-messages">
-					<?php if ( ! empty( $messages ) && is_array( $messages ) ) : ?>
-						<?php foreach( $messages as $message ) : ?>
-							<p><?php esc_html_e( $message ); ?></p>
-						<?php endforeach; ?>
-					<?php endif; ?>
-				</div>
-				<label for="swp_password">Please enter the site password</label>
-				<input type="password" name="swp_password" />
-				<?php wp_nonce_field( 'swp_password_submission', 'swp_wpnonce'); ?>
-				<input type="hidden" name="swp_action" value="submit_password" />
-				<input type="submit" value="Submit"/>
-			</form>
-		</div>
-	</main>
+    <main class="content-area content-area-full-width" role="main">
+        <div class="post-well">
+            <h2>Password Protected</h2>
+            <form method="post">
+                <div class="swp-messages">
+                    <?php if ( ! empty( $messages ) && is_array( $messages ) ) : ?>
+                        <?php foreach( $messages as $message ) : ?>
+                            <p class="alert alert-warning" role="alert"><?php esc_html_e( $message ); ?></p>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </div>
+                <fieldset>
+                    <label for="swp_password">Please enter the site password</label>
+                    <input type="password" id="swp_password" name="swp_password" />
+                </fieldset>
+                <?php wp_nonce_field( 'swp_password_submission', 'swp_wpnonce'); ?>
+                <input type="hidden" name="swp_action" value="submit_password" />
+                <div class="form-submit">
+                    <button type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    </main>
 </div>
 <?php get_footer();
